46947f52844d62393e2533c5e307cc964ddbd77f
AdamSulek <adam-s91@o2.pl>
2021-07-15 12:08:18 +0200

    adding new column in master_db

--- docker/dbt/models/working_hours_per_month/working_hours_per_month_model.sql
+++ docker/dbt/models/working_hours_per_month/working_hours_per_month_model.sql
@@ -4,12 +4,14 @@ with working_hours_per_month as (
     select
     name
     ,date_part('month', date) as "month"
+    ,date_part('year', date) as "year"
     ,sum(h) as worked_hours
     from {{source('metabase', 'master_db')}} as a
         left join {{source('metabase', 'timelogs')}} as b on a."user_ID"  = b."user"
     group by
     name
     ,date_part('month', date)
+    ,date_part('year', date)
 )
 
 select * from working_hours_per_month
