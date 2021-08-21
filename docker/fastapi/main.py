d6188bc165739c64df6ff926c724bbc484e293e3
AdamSulek <adam-s91@o2.pl>
2021-08-21 15:13:54 +0200

    adding pgadmin and dbt page redirecting

--- docker/fastapi/main.py
+++ docker/fastapi/main.py
@@ -18,7 +18,6 @@ class Timelogs(BaseModel):
     start_time: str = None
     end_time: str = None
     project_name: str = None
-    # date: str = None
     date: datetime = None
     h: float = None
     user: str = None
@@ -28,31 +27,28 @@ class Timelogs(BaseModel):
 def index(request: Request):
     return templates.TemplateResponse( "home.html", {"request": request} )
 
-# @app.post("/add-user/", response_class=HTMLResponse)
-# def add_new_user(request: Request):
-#     return templates.TemplateResponse( "add-user.html", {"request": request} )
-#
-#
-# @app.post("/add-timelogs/", response_class=HTMLResponse)
-# def add_new_user(request: Request):
-#     return templates.TemplateResponse( "add-timelogs.html", {"request": request} )
 
-
-@app.post("/add-user/")
-def add_new_user(id: str = Form(...),
+@app.post("/add-user/", response_class=HTMLResponse)
+def add_new_user(request: Request,
+                 id: str = Form(...),
                  first_name: str = Form(...),
                  last_name: str = Form(...),
                  email: str = Form(...)
                  ):
     db = Database()
     db.insert_into_master_db(id=id, first_name=first_name,
-                             last_name=last_name, email=email)
+                                    last_name=last_name, email=email)
+    # if user:
 
-    return {"first_name": first_name, "last_name": last_name}
+
+    return templates.TemplateResponse( "add-user.html", context={'request': request,
+                                                                 'first_name': first_name,
+                                                                 'last_name': last_name } )
 
 
-@app.post("/add-timelogs/")
-def add_new_timelogs(start_time: str = Form(...),
+@app.post("/add-timelogs/", response_class=HTMLResponse)
+def add_new_timelogs(request: Request,
+                     start_time: str = Form(...),
                      end_time: str = Form(...),
                      project_name: str = Form(...),
                      #date: str = Form(...),
@@ -71,18 +67,8 @@ def add_new_timelogs(start_time: str = Form(...),
     api_post_request["user"] = user # for now it is only my SLACK_ID, in future check list of available users
     api_post_request["ts"] = str(ts)
 
-
-    # message = {
-    #     "start_time": start_time,
-    #     "end_time": end_time,
-    #     "project_name": project_name,
-    #     "h": h,
-    #     "user": user,
-    #     "date": datetime.strptime(today_str, "%d.%m.%Y"),
-    #     "ts": str(ts)
-    # }
-
     db = Database(messages=api_post_request)
     db.insert_from_api()
 
-    return api_post_request
+    return templates.TemplateResponse( "add-timelogs.html", context={'request': request,
+                                                                     'h': h } )
